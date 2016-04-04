ad4a3506b10e3b1866305a7931448578709c77e7
Hillary Hintzen and Lindsey Raffa <lindsey@>
2016-04-04 16:47:37 -0700

    correct actions within our new listing

--- app/routes/category.js
+++ app/routes/category.js
@@ -4,15 +4,17 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('category', params.category_id);
   },
-
+ actions: {
   save3(params) {
-  var newListing = this.store.createRecord('listing', params);
-  var category = params.category;
-  category.get('listing').addObject(newListing);
-  newListing.save().then(function() {
-    return category.save();
-  });
-  this.transitionTo('category', params.city);
+
+    var newListing = this.store.createRecord('listing', params);
+    var category = params.category;
+    category.get('listing').addObject(newListing);
+    newListing.save().then(function() {
+      return category.save();
+    });
+    this.transitionTo('category', params.city);
+  }
 }
 
 });
