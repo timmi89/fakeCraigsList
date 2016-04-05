10465ad0544633b9fc647ba8b3febbfbba3b4298
Hillary Hintzen, Lindsey Raffa and Timmi McGuire <lindsey@>
2016-04-05 14:44:51 -0700

    moment added, not displaying

--- app/components/new-listing.js
+++ app/components/new-listing.js
@@ -1,5 +1,4 @@
 import Ember from 'ember';
-import moment from 'moment';
 
 export default Ember.Component.extend({
   addNewListing: false,
@@ -9,16 +8,16 @@ export default Ember.Component.extend({
     },
 
     save1() {
-      var date = new Date();
-      var time = date.toString().substring(16, 24);
-      date = date.toString().substring(4, 15);
-      var dateAndTime = date + ' ' + time;
-      var momentDateAndTime = moment(dateAndTime).format('MM/DD/YYYY hh:mm:ss a');
+      // var date = new Date();
+      // var time = date.toString().substring(16, 24);
+      // date = date.toString().substring(4, 15);
+      // var dateAndTime = date + ' ' + time;
+      // var momentDateAndTime = ;
       var params = {
         user: this.get('user'),
         title: this.get('title'),
         category: this.get('category'),
-        date: momentDateAndTime,
+        date: moment().format('MM/DD/YYYY hh:mm:ss a'),
         description: this.get('description'),
         cost: this.get('cost'),
         image: this.get('image')
--- app/helpers/dateTime.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-import moment from 'moment';
-
-export function currentDate(params/*, hash*/) {
-  Ember.Handlebars.registerBoundHelper('currentDate', function() {
-    return moment().format('LL');
-
-    // DO MOMENT AS HELPER?
